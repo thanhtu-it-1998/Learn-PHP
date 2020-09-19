8a74d74d018e23554e3ae3b25e24776d9e0eb128
It-Truongthanhtu <truongthanhtu.it.1998@gmail.com>
2020-09-19 09:51:43 -0700

    UPDATE: bai 2

---
+++ BASIC/Bai-2/index.php
@@ -0,0 +1,21 @@
+<?php
+    function test_variable(){
+        $num = 1;
+        echo $num;
+    }
+    function test_variable_1(){
+        global $num_1;
+        $num_1 = 2;
+        echo $num_1;
+    }
+    function test_variable_2(){
+        static $num_2;
+        $num_2 = 1;
+        $num_2++;
+        echo $num_2;
+    }
+    test_variable();
+    test_variable_1();
+    test_variable_2();
+    test_variable_2();    
+?>
